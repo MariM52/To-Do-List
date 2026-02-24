ca314fd7c6b1b2099f38d2eee9a2360996f6ae8f
Marianne <mariannemah@berkeley.edu>
2026-02-23 20:03:33 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Task from './Task.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Hello this is a To-Do List</h1>
+    <button> </button>
+    </>
+  )
+}
+
+export default App
---
+++ src/Task.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Task() {
+
+  return (
+    <>
+    <h1>This is the first task</h1>
+    </>
+  )
+}
+
+export default Task
