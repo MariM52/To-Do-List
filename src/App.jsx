794fe4a8ecada56bc16aba249aa4f82b3d6bf09f
Marianne <mariannemah@berkeley.edu>
2026-03-02 15:10:05 -0800

    basic todo w/ font

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import './App.css'
 import Task from './Task.jsx'
 
 function App() {
+  
   const [tasks, setTasks] = useState([
-      {name: 'cs61b proj', status: false, dueDate: "1/1/26"},
-      {name: 'calc hw', status: false, dueDate: "1/1/26"}
+      {name: 'CS61b proj', status: "Not Started", dueDate: "1/1/26"},
+      {name: 'Calc hw', status: "Not Started", dueDate: "1/1/26"}
   ]);
 
 
   return (
     <>
-    <h1>Hello this is a To-Do List</h1>
+    <div className='todo'>
+    <h1>My To-Do List</h1>
     {tasks.map((task) => (<Task
       name = {task.name}
       status = {task.status}
       dueDate = {task.dueDate} />
     ))}
+    </div>
     </>
   )
 }
