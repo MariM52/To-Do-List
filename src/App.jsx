7af571c5fbec7d7c4bd0800458367776dd755b06
Marianne <mariannemah@berkeley.edu>
2026-03-02 21:21:33 -0800

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Task from './Task.jsx'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
 
 function App() {
-  
-  const [tasks, setTasks] = useState([
-      {name: 'CS61b proj', status: "Not Started", dueDate: "1/1/26"},
-      {name: 'Calc hw', status: "Not Started", dueDate: "1/1/26"}
-  ]);
-
-
   return (
     <>
-    <div className='todo'>
-    <h1>My To-Do List</h1>
-    {tasks.map((task) => (<Task
-      name = {task.name}
-      status = {task.status}
-      dueDate = {task.dueDate} />
-    ))}
-    </div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
     </>
   )
 }
